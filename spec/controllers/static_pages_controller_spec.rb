4686be7fd57444179ca9ca0085b1a33588fddee9
lsfernandes92 <lsfernandes92@gmail.com>
2021-12-09 21:36:21 -0300

    Update tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,15 +2,22 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe 'GET #home' do
-    it 'should get home' do
+    it 'should get home page' do
       get :home
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET #help' do
-    it 'should get help' do
+    it 'should get help page' do
       get :help
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #about' do
+    it 'should get about page' do
+      get :about
       expect(response).to have_http_status(:success)
     end
   end
