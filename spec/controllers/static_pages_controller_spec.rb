cd60442a46bb911111bafbc6e07446710471996a
lsfernandes92 <lsfernandes92@gmail.com>
2021-12-09 21:51:08 -0300

    Add test for the new contact page

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -21,4 +21,11 @@ RSpec.describe StaticPagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'GET #contact' do
+    it 'should get contact page' do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
