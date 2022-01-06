d52188e5b796da42fe871d7d0cdbf27bc6b3dcaa
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-06 17:10:01 -0300

    Add admin column to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ User.create!(
   name: 'Lucas Fernandes',
   email: 'lsfernandes92@gmail.com',
   password: 'a123456',
-  password_confirmation: 'a123456'
+  password_confirmation: 'a123456',
+  admin: true
 )
 
 99.times do |n|
