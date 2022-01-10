ab1564afe7c75f004b2d15263a04534f28e16a77
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-10 15:25:02 -0300

    Implement basic image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_014008) do
+ActiveRecord::Schema.define(version: 2022_01_10_181431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_01_09_014008) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
