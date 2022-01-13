89b00552efa013cb2879cb6345dddd001cb5522d
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-13 10:58:23 -0300

    Increment seeds adding the followers and followings to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,3 +54,12 @@ users = User.order(:created_at).take(6)
   users.each {|user| user.microposts.create!(content: content) }
 end
 puts "%%%% Microposts created successfully!"
+
+puts "%%%% Associating user followers and followings..."
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+puts "%%%% Followers and followings created successfully!"
